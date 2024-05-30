f0b7ebdc09bae024170bc372b733e1480571fbdd
Timur Tursunov <t.timon2003@gmail.com>
2024-05-30 21:31:25 +0300

    qwerty

---
+++ main.py
@@ -0,0 +1,54 @@
+#1
+# a = int(input("Введите ваше число? "))
+# for i in range(1, a + 1):
+#     print(i)
+
+
+#2
+# a = input("Write your word or number ")
+# print(a[::-1])
+
+
+#3
+
+# a = int(input("Введите ваше число "))
+# for i in range(1, 10):
+#     print(a,"*",i,"=", a * i)
+#4
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# x = 0
+# y = 0
+# for i in range(a, b, 2):
+#     x += 1
+# print('нечетные: ', x)
+# a += 1
+# for i in range(a, b, 2):
+#     y += 1
+# print('четные: ', y)
+
+#5
+
+# import random
+#
+# a = random.randrange(1, 100)
+# print("Я загадал число от 1 до 100")
+# while True:
+#     v = int(input("Введите число "))
+#     if v > a:
+#         print("Меньше")
+#     elif v < a:
+#         print("Больше")
+#     elif v == a:
+#         print("Правильно")
+#         break
+
+
+
+#6
+# a = int(input("Введите ваше число? "))
+# for x in range(1, a ):
+#     print('x' * x)
+
+
